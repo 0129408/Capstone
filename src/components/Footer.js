327d073afc80cfd38938d5070e5a2ce18b2e365c
Lee <0129408@kdu-online.com>
2023-04-18 18:22:53 +0800

    yes :wq

---
+++ src/components/Footer.js
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <footer>
+      <menu>
+        <li className="img-footer">
+          <img
+            src={require("../assets/footer-logo.png")}
+            alt="Little Lemon logo"
+          ></img>
+        </li>
+        <li className="contact">
+          <h1 className="footer-header">Navigation</h1>
+          <ul className="footer-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <a
+              href={require("../assets/Menu.png")}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Menu
+            </a>
+            <Link to="/reservations">Reservations</Link>
+            <Link to="/order">Order</Link>
+            <Link to="/login">Login</Link>
+          </ul>
+        </li>
+
+        <li>
+          <h1 className="footer-header">Contact</h1>
+          <ul className="footer-links">
+            <li>1235 Maple Avenue, Suite 205</li>
+            <li>Hillcrest, CA 90210</li>
+            <br></br>
+            <li>(123)-456-6789</li>
+            <br></br>
+            <a
+              href="mailto: info@littlelemon.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              info@littlelemonrestaurant.com
+            </a>
+          </ul>
+        </li>
+        <li>
+          <h1 className="footer-header">Connect</h1>
+          <ul className="footer-links">
+            <a
+              href="https://www.facebook.com/thelittlelemonrestaurant/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.instagram.com/littlelemonrestaurant/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </ul>
+        </li>
+      </menu>
+    </footer>
+  );
+}
